d901fa4bfe475fbd960bf97b3eeed37f554c33b2
Harmehar-kaur <kaurharmehar9717@gmail.com>
2023-11-19 16:58:15 +0530

    added angular

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -25,7 +25,11 @@ export class PostsService {
 
   addPost(title: string, content: string) {
     const post: Post = { title: title, content: content, id: ''};
-    this.posts.push(post);
+    this.http.post<{message:string}>('http://localhost:3000/api/posts', post)
+    .subscribe((responseData) =>{
+      console.log(responseData.message);
+      this.posts.push(post);
     this.postsUpdated.next([...this.posts]);
+    }); 
   }
 }
